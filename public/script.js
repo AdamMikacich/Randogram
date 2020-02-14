2e2abf220b994cfa8c01d9177aac8118e0595756
manazari <matthew.nazari20@bcp.org>
2020-02-14 12:27:35 -0800

    image zooming

--- public/script.js
+++ public/script.js
@@ -10,6 +10,24 @@ document.addEventListener('DOMContentLoaded', () => {
     element.innerHTML = `<img src="./img?${imageID++}">`;
 
     images.appendChild(element);
+
+    element.style.width = '500px';
+    element.style.height =  '500px';
+    element.setAttribute('data-zoomed', 'false');
+
+    element.addEventListener('click', () => {
+      console.log('click' + element.getAttribute('data-zoomed'));
+      if (element.getAttribute('data-zoomed') === 'true') {
+        element.style.width = '500px';
+        element.style.height =  '500px';
+        element.setAttribute('data-zoomed', 'false');
+      } else {
+        console.log('YEAAA, zooming in... NOW!');
+        element.style.width = element.childNodes[0].naturalWidth + 'px';
+        element.style.height = element.childNodes[0].naturalHeight + 'px';
+        element.setAttribute('data-zoomed', 'true');
+      }
+    });
   }
 
   function addImagesMultiple() {
@@ -22,7 +40,7 @@ document.addEventListener('DOMContentLoaded', () => {
   addImagesMultiple();
 
   window.addEventListener('scroll', (e) => {
-    if (window.scrollY + window.innerHeight === images.offsetHeight) {
+    if (window.scrollY + window.innerHeight > images.offsetHeight - 100) {
       addImagesMultiple();
     }
   })
